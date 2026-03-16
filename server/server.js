4c262b4c27b101c3d07afb50b9c99ef9e1de2774
Phan Như Nhật <phannhunhat1234@gmail.com>
2026-03-16 18:00:17 +0700

    Update

--- server/server.js
+++ server/server.js
@@ -19,7 +19,11 @@ const statisticsRoutes = require("./routes/statisticsRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
 const bestsellerRoutes = require("./routes/bestsellerRoutes");
 
-const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://bike-ecommerce-frontend-client.vercel.app",
+];
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
@@ -31,7 +35,7 @@ app.use(
       }
     },
     credentials: true,
-  })
+  }),
 );
 
 app.use(express.json());
@@ -39,7 +43,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   "/uploads/images",
-  express.static(path.join(__dirname, "uploads/images"))
+  express.static(path.join(__dirname, "uploads/images")),
 );
 
 app.use("/api/posts", postRoutes);
@@ -59,7 +63,7 @@ app.use("/api/admin/statistics", statisticsRoutes);
 
 app.use(
   "/uploads/images",
-  express.static(path.join(__dirname, "uploads/images"))
+  express.static(path.join(__dirname, "uploads/images")),
 );
 
 app.listen(PORT, () => {
