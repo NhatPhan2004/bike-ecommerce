7913ea867e1e913037d4326c370edf49889d13b5
Phan Như Nhật <phannhunhat1234@gmail.com>
2026-03-16 10:22:36 +0700

    Update database

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 console.log("DB_HOST:", process.env.DB_HOST);
@@ -7,10 +7,11 @@ console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "***" : "(empty)");
 console.log("DB_NAME:", process.env.DB_NAME);
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "quanlyxedap",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
